910686be68e6101b723a8036a9a7c6d6ef6e68f5
Alexandra Lee <alexandraelee9@gmail.com>
2024-08-12 16:14:34 +0000

    create database with models.py file

---
+++ plantapp/models.py
@@ -0,0 +1,25 @@
+from plantapp import db
+from datetime import datetime
+
+#table for categories
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(20), unique=True, nullable=False)
+    recipes = db.relationship("Recipe", backref="category", cascade="all, delete", lazy=True )
+
+    def __repr__(self):
+        return f'<Category {self.category_name}>'
+
+#table for recipes
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(150), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        return f'<Recipe {self.recipe_name}>'
--- plantapp/routes.py
+++ plantapp/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
 from plantapp import app, db
+from plantapp.models import Category, Recipe
 
 
 @app.route("/")
